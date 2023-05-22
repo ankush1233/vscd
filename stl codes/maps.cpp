60896f97aa14f6cb0e47701145a7428618cc6004
Ankush Mondal <ankushmondal1y2t@gmail.com>
2023-05-22 17:28:54 +0530

    new file:   bit/bti1.cpp

---
+++ stl codes/maps.cpp
@@ -0,0 +1,22 @@
+// maps
+// INSERTION O(log(n))
+//same for deletion and printing
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    map<int, string> m;
+    m[1] = "abk";
+    m[10] = "joj";
+    m[3] = "dhi";
+    m.insert({4, "ldl"});
+    //map<int, string> :: iterator it;
+   /* for(it = m.begin(); it != m.end(); it++){
+        cout<<(*it).first<<" "<<(*it).second<<endl;
+    }*/
+    for(auto &p : m){
+        cout<<p.first<<" "<<p.second<<endl;      
+    }
+
+}
--- stl codes/stlvector.cpp
+++ stl codes/stlvector.cpp
@@ -2,17 +2,16 @@
 
 using namespace std;
 int main(){
-    /*vector<int> A={1,2,32,8,7,43,76};
+   /* vector<int> A={1,2,32,8,7,43,76};
     
     sort(A.begin(),A.end());
     for(auto show:A){
         cout<<show<<" ";
     }
-    return 0;
-    */
-    /*
+    return 0;*/
+    
     int arr[4];
-    memset(arr, -1,sizeof(arr));   //fill the full array with -1 only works for -1 ;
+    memset(arr, 0,sizeof(arr));   //fill the full array with -1 only works for -1 ;
     cout<<arr[3];
-    */
+    
 }
